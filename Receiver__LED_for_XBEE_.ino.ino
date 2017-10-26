bcc92c51ba0266a7c9ac2d40316d18d5181049b6
dilano1 <32854736+dilano1@users.noreply.github.com>
2017-10-25 17:20:32 -0700

    Add files via upload

---
+++ Receiver__LED_for_XBEE_.ino.ino
@@ -0,0 +1,34 @@
+#include <Printers.h>
+#include <XBee.h>
+
+int myData = 0;
+int const ledpin = 13;
+
+void setup(){
+  // Start up our serial port, we configured our XBEE devices for 9600 bps.
+  Serial.begin(9600);
+  pinMode(ledpin, OUTPUT);
+}
+
+void loop(){
+   
+  if(Serial.available() > 0){
+   
+      myData = Serial.read();
+     
+      if(myData == '1'){
+      digitalWrite(ledpin, HIGH);
+      Serial.write(myData);
+      }
+     
+      if(myData == '0'){
+      digitalWrite(ledpin, LOW);
+      Serial.write(myData);
+  }
+  else{
+      Serial.write(myData);
+  }
+   
+}
+}
+
